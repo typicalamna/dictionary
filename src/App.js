21b5184439b39c4807873b6459eed19ade23410e
typicalamna <typicalamna@gmail.com>
2021-05-15 19:05:51 -0400

    change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from "./logo.png"
+import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <img src={logo} className="App-logo img-fluid"
        alt="logo" /> 
       </header>
+      <main>
+        <Dictionary />
+      </main>
+
+
+      <footer className="App-footer"><small> Coded By Amna Malik</small>
+      </footer>
+      </div>
     </div>
   );
 }
