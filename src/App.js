bbdf65e96efbea397f85865a7f1cb1e28fba07b2
typicalamna <typicalamna@gmail.com>
2021-05-16 00:32:58 -0400

    Name

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+          <small>Coded by Amna Malik</small>
         </footer>
       </div>
     </div>
